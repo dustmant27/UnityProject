dc8af61e11b3af9a20210b0cf6e7684c4a91536a
dustmant27 <drbridgewate@gmail.com>
2014-01-12 10:16:19 -0500

    content- rough main menu, camera feed
    First rough main menu created, when button clicked your camera displays
    whatever is shown on the orb. This is going to be used in the game..

---
+++ Week1-2Project/Assets/changeTexture.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeTexture : MonoBehaviour {
+
+	public void doChange(){
+		string deviceName = WebCamTexture.devices[0].name;
+		
+		WebCamDevice[] devices = WebCamTexture.devices;
+		string frontCamName="";
+		for( int i = 0 ; i < devices.Length ; i++ ) {
+			Debug.Log("Device:"+devices[i].name+ "IS FRONT FACING:"+devices[i].isFrontFacing);
+			
+			if (devices[i].isFrontFacing) {
+				frontCamName = devices[i].name;
+			}
+		}
+		WebCamTexture  webCamTexture = new WebCamTexture(frontCamName, 1280, 720, 30);
+		
+		webCamTexture.Play();
+		renderer.material.mainTexture = webCamTexture;
+	}
+}
---
+++ Week1-2Project/Assets/mainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenu : MonoBehaviour {
+	private changeTexture targetBall;
+
+	// Use this for initialization
+	void OnGUI(){
+		string latLon;
+		LocationInfo location = new LocationInfo();
+
+		targetBall = GameObject.FindGameObjectWithTag("Player").GetComponent<changeTexture>();
+		GUI.Box (new Rect ((Screen.width/2) - 50, (Screen.height /2) - 50, 200, 190), "Main Menu");
+		if(GUI.Button (new Rect((Screen.width/2) - 25, (Screen.height /2) - 25, 150, 125), "Play")){
+			
+			targetBall.doChange ();
+		}
+		latLon = location.latitude.ToString()  + ", " +location.longitude.ToString() ;
+	
+		GUI.Label (new Rect ((Screen.width / 2) - 25, (Screen.height - 25), 50, 25), latLon);
+		}
+
+
+}
