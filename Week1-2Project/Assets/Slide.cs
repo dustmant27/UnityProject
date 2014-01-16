aece0e1bc5f7e644039547f29da4a1c103b62d46
dustmant27 <drbridgewate@gmail.com>
2014-01-15 23:22:18 -0500

    Main menu and initial game screen
    Physics, tilt control, gps coords all implemented in a basic fashion.
    Can go from main menu to game screen but not back yet.

---
+++ Week1-2Project/Assets/Slide.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slide : MonoBehaviour {
+ float speed= 15f;
+ float moveThreshold= .5f;
+	
+	 float movex; 
+	float movey;
+	 float iPx; 
+	float iPy;
+	void Update()
+	{
+		Vector3 dir = Vector3.zero;
+		// we assume that device is held parallel to the ground
+		// and Home button is in the right hand
+		
+		// remap device acceleration axis to game coordinates:
+		//  1) XY plane of the device is mapped onto XZ plane
+		//  2) rotated 90 degrees around Y axis
+		dir.x = Input.acceleration.x;
+		if (Mathf.Abs (Input.acceleration.y) > moveThreshold) {
+			dir.z = Input.acceleration.y;
+
+				}
+		
+		// clamp acceleration vector to unit sphere
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
+		
+		// Make it move 10 meters per second instead of 10 meters per frame...
+		dir *= Time.deltaTime;
+		
+		// Move object
+		transform.Translate (dir * speed);
+	}
+}
--- Week1-2Project/Assets/mainMenu.cs
+++ Week1-2Project/Assets/mainMenu.cs
@@ -3,22 +3,25 @@ using System.Collections;
 
 public class mainMenu : MonoBehaviour {
 	private changeTexture targetBall;
+	private int buttonW = 100;
+	private int buttonH = 100;
+	private float halfScreenW = Screen.width/2;
+	public GUISkin customSkin;
 
-	// Use this for initialization
 	void OnGUI(){
 		string latLon;
 		LocationInfo location = new LocationInfo();
-
-		targetBall = GameObject.FindGameObjectWithTag("Player").GetComponent<changeTexture>();
-		GUI.Box (new Rect ((Screen.width/2) - 50, (Screen.height /2) - 50, 200, 190), "Main Menu");
-		if(GUI.Button (new Rect((Screen.width/2) - 25, (Screen.height /2) - 25, 150, 125), "Play")){
-			
-			targetBall.doChange ();
+		GUI.skin = customSkin;
+		iPhoneSettings.StartLocationServiceUpdates();
+		//        targetBall = GameObject.FindGameObjectWithTag("Player").GetComponent<changeTexture>();
+		//GUI.Box (new Rect ((Screen.width/2) - 50, (Screen.height /2) - 50, 200, 190), "Main Menu");
+		if(GUI.Button (new Rect(halfScreenW - (buttonW/2)  , Screen.height - 150, 150, buttonH), "Play")){
+			Application.LoadLevel ("GameLevel");
+			//targetBall.doChange ();
 		}
-		latLon = location.latitude.ToString()  + ", " +location.longitude.ToString() ;
+		latLon = iPhoneInput.lastLocation.latitude.ToString()  + ", " +iPhoneInput.lastLocation.longitude.ToString () ;
+		
+		GUI.Label (new Rect ((Screen.width / 2) - 75,  25, 250, 150), latLon);
+	}
 	
-		GUI.Label (new Rect ((Screen.width / 2) - 25, (Screen.height - 25), 50, 25), latLon);
-		}
-
-
 }
